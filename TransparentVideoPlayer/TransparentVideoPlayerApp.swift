37f68bda26cc65b39e5a6888b6c7f7bbf8e0c7ac
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2026-01-02 20:10:11 +0530

    Initial Commit

---
+++ TransparentVideoPlayer/TransparentVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentVideoPlayerApp.swift
+//  TransparentVideoPlayer
+//
+//  Created by Om Chachad on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TransparentVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
